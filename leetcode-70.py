caac8792024d11500f079457f60affee001c9694
CodingInMyQuilt <3665390758@qq.com>
2025-01-10 22:50:20 +0800

    overtime

--- leetcode-70.py
+++ leetcode-70.py
@@ -1,5 +1,15 @@
-def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+def climbStairs(n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    def recur(n, m=0):
+        if n > 0:
+            return recur(n-1) + recur(n-2)
+        elif n == 0:
+            return m + 1
+        else:
+            return 0
+    return recur(n)
+
+print(climbStairs(3))
