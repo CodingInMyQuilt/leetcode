701f7d712de64d618cfb6eed74c7d31955d85cbe
CodingInMyQuilt <3665390758@qq.com>
2025-01-10 14:04:12 +0800

    pass

--- leetcode-daily-3297.py
+++ leetcode-daily-3297.py
@@ -5,23 +5,32 @@ def validSubstringCount(word1, word2):
         :rtype: int
         """
         d2 = {}
-        dd2 = []
         r = []
+        a = 0
         for i in word2:
             if d2.get(i) is None:
                 d2[i] = 1
             else:
                 d2[i] += 1
         for i in range(len(word1)):
-            dd2.append(d2.copy())
             if d2.get(word1[i]) is not None:
-                for j in range(len(dd2) - 1, -1, -1):
-                    if dd2[j][word1[i]] > 0:
-                        dd2[j][word1[i]] -= 1
-                    if sum(dd2[j].values()) == 0:
-                        del dd2[j]
+                d2[word1[i]] -= 1
+                x = 1
+                for j in d2.values():
+                    if j > 0:
+                        x = 0
+                        break
+                if x == 1:
+                    while x == 1:
+                        if d2.get(word1[a]) is not None:
+                            d2[word1[a]] += 1
+                        a += 1
                         r.append(i)
+                        for j in d2.values():
+                            if j > 0:
+                                x = 0
+                                break
         r = [len(word1) - n for n in r]
         return sum(r)
 
-print(validSubstringCount("dcbdcdccb", "cdd"))
+print(validSubstringCount("bcca", "abc"))
