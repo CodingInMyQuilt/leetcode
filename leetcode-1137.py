05954b73eefa28aad7c5b78e788c909605c6b388
CodingInMyQuilt <3665390758@qq.com>
2025-01-14 21:54:11 +0800

    pass

--- leetcode-1137.py
+++ leetcode-1137.py
@@ -3,4 +3,20 @@ def tribonacci(n):
     :type n: int
     :rtype: int
     """
-    
+    a = 0
+    b = 1
+    c = 1
+    if n == 0: 
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        x = 0
+        for i in range(3, n+1):
+            x = a + b + c
+            a = b
+            b = c
+            c = x
+        return x
+
+print(tribonacci(3))
